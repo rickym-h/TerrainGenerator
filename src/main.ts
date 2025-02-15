bf244ce88811b482b57d6df4f69cfaba1d69835a
ricky <richardhull11@gmail.com>
2025-02-15 19:47:29 +0000

    - seperated tick and initialisation into separate functions - added a top-down map controls for the camera - added basic lighting for the scene

--- src/main.ts
+++ src/main.ts
@@ -1,25 +1,76 @@
 import * as THREE from 'three';
+import { MapControls } from 'three/addons/controls/MapControls.js';
 
-const scene = new THREE.Scene();
-const camera = new THREE.PerspectiveCamera( 75, window.innerWidth / window.innerHeight, 0.1, 1000 );
+let camera: THREE.PerspectiveCamera, controls: MapControls, scene: THREE.Scene, renderer: THREE.WebGLRenderer;
 
-const renderer = new THREE.WebGLRenderer();
-renderer.setSize( window.innerWidth, window.innerHeight );
-renderer.setAnimationLoop( animate );
-document.body.appendChild( renderer.domElement );
+init();
 
-const geometry = new THREE.BoxGeometry( 1, 1, 1 );
-const material = new THREE.MeshBasicMaterial( { color: 0x00ff00 } );
-const cube = new THREE.Mesh( geometry, material );
-scene.add( cube );
+function init() {
+    scene = new THREE.Scene();
+    scene.background = new THREE.Color( 0xcccccc );
+    scene.fog = new THREE.FogExp2( 0xcccccc, 0.001 );
 
-camera.position.z = 5;
+    renderer = new THREE.WebGLRenderer( { antialias: true } );
+    renderer.setPixelRatio( window.devicePixelRatio );
+    renderer.setSize( window.innerWidth, window.innerHeight );
+    renderer.setAnimationLoop( tick );
+    // @ts-ignore
+    document.getElementById("app").appendChild( renderer.domElement );
 
-function animate() {
 
-    cube.rotation.x += 0.01;
-    cube.rotation.y += 0.01;
+    camera = new THREE.PerspectiveCamera( 60, window.innerWidth / window.innerHeight, 1, 2000 );
+    camera.position.set( 0, 200, - 400 );
 
+    // controls
+    controls = new MapControls( camera, renderer.domElement );
+    controls.enableDamping = true; // an animation loop is required when either damping or auto-rotation are enabled
+    controls.dampingFactor = 0.05;
+    controls.screenSpacePanning = false;
+    controls.minDistance = 100;
+    controls.maxDistance = 500;
+    controls.maxPolarAngle = Math.PI / 2;
+
+    // world
+    const geometry = new THREE.BoxGeometry();
+    const material = new THREE.MeshPhongMaterial( { color: 0xeeeeee, flatShading: true } );
+
+    const mesh = new THREE.Mesh( geometry, material );
+    mesh.scale.x = 100;
+    mesh.scale.y = 100;
+    mesh.scale.z = 100;
+    mesh.updateMatrix();
+    scene.add( mesh );
+
+    // lights
+    const dirLight1 = new THREE.DirectionalLight( 0xffffff, 3 );
+    dirLight1.position.set( 1, 1, 1 );
+    scene.add( dirLight1 );
+
+    const dirLight2 = new THREE.DirectionalLight( 0x002288, 3 );
+    dirLight2.position.set( - 1, - 1, - 1 );
+    scene.add( dirLight2 );
+
+    const ambientLight = new THREE.AmbientLight( 0x555555 );
+    scene.add( ambientLight );
+
+    //
+    window.addEventListener( 'resize', onWindowResize );
+
+}
+
+function onWindowResize() {
+    camera.aspect = window.innerWidth / window.innerHeight;
+    camera.updateProjectionMatrix();
+
+    renderer.setSize( window.innerWidth, window.innerHeight );
+}
+
+function tick() {
+    controls.update(); // only required if controls.enableDamping = true, or if controls.autoRotate = true
+
+    render();
+}
+
+function render() {
     renderer.render( scene, camera );
-
 }
